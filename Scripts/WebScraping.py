78d66016f03f5ffd98083549ce53e3703844db15
lruizap <lruizap2502@gmail.com>
2024-01-15 20:42:04 +0100

    finished scrape and json with contents

--- Scripts/WebScraping.py
+++ Scripts/WebScraping.py
@@ -1,72 +1,96 @@
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 url = "https://www.adideandalucia.es/index.php?view=normativa"
 arc_json = '../JSON/ConserGPT_DATA.json'
 
-# Configura el controlador del navegador (asegúrate de tener el controlador de Chrome instalado)
-chrome_path = "../chromedriver.exe"
-service = ChromeService(chrome_path)
+finalJSON = {}
 
-# First page
-driver = webdriver.Chrome(service=service)
+try:
+    # Configura las opciones del navegador para deshabilitar la carga de imágenes
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-images")
+    chrome_options.add_argument("--disable-extensions")
 
-# Abre la página web
-driver.get(url)
+    # Configura el controlador del navegador con las opciones
+    chrome_path = "../chromedriver.exe"
+    service = ChromeService(chrome_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Espera a que el contenido dinámico generado por JavaScript cargue completamente
-# Puedes ajustar este tiempo según sea necesario
-driver.implicitly_wait(10)
+    # Abre la página web
+    driver.get(url)
 
-# Encuentra la tabla por algún identificador, clase, etc.
+    # Espera a que el contenido dinámico generado por JavaScript cargue completamente
+    # Puedes ajustar este tiempo según sea necesario
+    driver.implicitly_wait(10)
 
-module = driver.find_elements(By.XPATH, "//div[@class='module-inner']")
+    # Encuentra la tabla por algún identificador, clase, etc.
+    module_elements = driver.find_elements(
+        By.XPATH, "//div[@class='module-inner']")
 
-
-def datosTitle():
-    finalJSON = {}
     # Recorre Todos los Modulos
-    for oneModuleInner in module:
+    for oneModuleInner in module_elements:
         # Obtiene La letra
-        letter = oneModuleInner.find_element(By.TAG_NAME, 'h3')
-        finalJSON["Letter"] = letter.text
-        # Obtiene el resto
-        tableI = oneModuleInner.find_element(By.TAG_NAME, 'tbody')
+        letter = oneModuleInner.find_element(By.TAG_NAME, 'h3').text
 
-        print("Trabajando con: ", letter.text)
+        # Si la letra no existe en finalJSON, crea un nuevo diccionario
+        if len(letter) == 1:
+            if letter not in finalJSON:
+                finalJSON[letter] = {"Info": []}
 
-        if oneModuleInner:
-            # print(letter.text)
-            for tr in tableI.find_elements(By.TAG_NAME, 'tr'):
-                # print("Entrando al tr...")
-                for td in tr.find_elements(By.TAG_NAME, 'td'):
-                    # print("Entrando al td...")
-                    for a in td.find_elements(By.TAG_NAME, 'a'):
-                        finalJSON["Info"] = {}
-                        finalJSON["Info"]["Title"] = a.text
-                        finalJSON["Info"]["SectionLink"] = a.get_property(
-                            'href')
-        else:
-            return finalJSON
+            # Obtiene el resto
+            tableI = oneModuleInner.find_element(By.TAG_NAME, 'tbody')
 
-        # Cierra la página web
-        driver.close()
+            print("Trabajando con: ", letter)
 
-        # for h3 in h3s:
-        # if len(h3.text) == 1:
-        # datos_title.append(h3.text)
+            if oneModuleInner:
+                for tr in tableI.find_elements(By.TAG_NAME, 'tr'):
+                    for td in tr.find_elements(By.TAG_NAME, 'td'):
+                        for a in td.find_elements(By.TAG_NAME, 'a'):
+                            link = a.get_property('href')
+                            title = a.text
 
+                            info_data = {"Title": title,
+                                         "SectionLink": link, "ListPDF": []}
 
-with open(arc_json, 'w') as archivo:
-    archivo.write(datosTitle())
+                            try:
+                                # Second page
+                                driver2 = webdriver.Chrome(service=service)
+                                driver2.get(link)
+                                driver2.implicitly_wait(10)
+                                moduleGlobal = driver2.find_element(
+                                    By.XPATH, "//ul[@class='lista']")
 
-print(datosTitle())
-# Second page
-# Abrir y cerrar cuando se use
-# driverInside = webdriver.Chrome(service=service)
+                                for li in moduleGlobal.find_elements(By.TAG_NAME, 'li'):
+                                    liText = li.text
+                                    for a in li.find_elements(By.TAG_NAME, 'a'):
+                                        pdfLink = a.get_property('href')
 
-# Cierra la página web
-driver.close()
-# driverInside.close()
+                                    pdfTotal = {
+                                        "Text": liText,
+                                        "LinkPDF": pdfLink,
+                                    }
+
+                                    info_data["ListPDF"].append(pdfTotal)
+
+                                driver2.close()
+
+                            except Exception as e:
+                                print(f"Error en la segunda página: {e}")
+                                break
+
+                            finalJSON[letter]["Info"].append(info_data)
+
+    # Cierra la página web
+    driver.close()
+
+    with open(arc_json, 'w') as archivo:
+        json.dump({"modules": finalJSON}, archivo)
+
+except Exception as e:
+    with open(arc_json, 'w') as archivo:
+        json.dump({"modules": finalJSON}, archivo)
+    print(f"Error general: {e}")
